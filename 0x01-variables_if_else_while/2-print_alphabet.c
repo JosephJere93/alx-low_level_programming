95df35de9fa595883e5e5afade7b53631a75993a
JosephJere93 <josephjere93@gmail.com>
2023-03-20 09:32:16 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    const char alp[] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    for (i = 0; i < sizeof(alp); i++)
+    {
+        putchar(alp[i]);
+    }
+    putchar('\n');
+
+    return (0);
+}
