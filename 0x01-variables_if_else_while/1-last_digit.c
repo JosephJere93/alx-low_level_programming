b985ecfb3b8015d8b734fed48a2fae9317ac2d05
JosephJere93 <josephjere93@gmail.com>
2023-03-20 09:21:47 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,30 +9,30 @@
  */
 int main(void)
 {
-        int n;
+	int n;
+	
+	/* seed the random number generator */
+	srand(time(0));
 
-        /* seed the random number generator */
-        srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        n = rand() - RAND_MAX / 2;
+	/* check if the last digit is greater than 5 */
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
 
-        /* check if the last digit is greater than 5 */
-        if ((n % 10) > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-        }
+	/* check if the last digit is less than 6 and not 0 */
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
 
-        /* check if the last digit is less than 6 and not 0 */
-        else if ((n % 10) < 6 && (n % 10) != 0)
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-        }
-
-        /* if the last digit is 0 */
-        else
-        {
-                printf("Last digit of %d is %d and is 0\n", n, n % 10);
-        }
+	/* if the last digit is 0 */
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
 
         return (0);
 }
