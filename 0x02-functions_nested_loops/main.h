dd79aaf5a998963ee3562a94f5e854d69a041c99
JosephJere93 <josephjere93@gmail.com>
2023-03-22 23:44:42 -0700

    Task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_10x(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -9,4 +9,4 @@ int print_last_digit(int);
 void jackbauer(void);
 int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
+void print_times_table(int n);
