213e31bbe0be410d8a30d921d0d0f371d7510424
JosephJere93 <josephjere93@gmail.com>
2023-03-24 02:54:59 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-        int i;
-        printf("Infinite loop incoming :(\n");
+	int i;
 
-        i = 0;
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
 
 	/*
 	 * while (i < 10)
@@ -18,7 +19,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
-        printf("Infinite loop avoided! \\o/\n");
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
