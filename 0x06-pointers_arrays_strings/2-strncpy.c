9e6c96ad022caf50163c12a294ebb97b72b9f9bb
JosephJere93 <josephjere93@gmail.com>
2023-03-31 02:39:25 -0700

    Task 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,21 +7,22 @@
  *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-        int j;
+	int j;
 
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-                dest[j] = src[j];
-                j++;
-        }
-        while (j < n)
-        {
-                dest[j] = '\0';
-                j++;
-        }
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-        return (dest);
+	return (dest);
 }
