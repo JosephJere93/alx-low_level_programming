42ee3aa2ad36e11f5c148bed15718944c2d1169b
JosephJere93 <josephjere93@gmail.com>
2023-03-31 03:03:16 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,15 +6,16 @@
  *
  * Return: void
  */
+
 void reverse_array(int *a, int n)
 {
-        int i;
-        int t;
+	int i;
+	int t;
 
-        for (i = 0; i < n--; i++)
-        {
-                t = a[i];
-                a[i] = a[n];
-                a[n] = t;
-        }
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
 }
