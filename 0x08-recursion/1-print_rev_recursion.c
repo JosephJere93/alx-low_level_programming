93d898447cc6deea89d3e6f829396a818f3204ba
JosephJere93 <josephjere93@gmail.com>
2023-04-06 06:41:01 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /*
- * _print_rev_recursion
+ * _print_rev_recursion - print reverse
  * @s: string
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 			_putchar(*s);
