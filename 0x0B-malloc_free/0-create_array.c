90d4a7f95ab7f9016b842c7d32c973d443c908b6
JosephJere93 <josephjere93@gmail.com>
2023-04-11 05:27:56 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * 0-create_array - create_array(unsigned int size, char c)
  * @size: size of array
  * @c: assigned char
- * Desc: array of size size and assign char c
+ * Description: create array of size size and assign char c
  * Return: point array, NULL if not success
  */
 
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
-	
+
 	str = malloc(sizeof(char) * size);
 	if(size == 0 || str == NULL)
 		return(NULL);
