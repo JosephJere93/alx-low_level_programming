edea6e4d505678d105ad6fb5b05e25c0f8186d57
JosephJere93 <josephjere93@gmail.com>
2023-04-16 23:18:50 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-calloc.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _memtest - allocates memory for an array, using malloc.
+ * @s: memory
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to memory
+ */
+
+char *_memtest(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * *_calloc - memory for array
+ * @nmemb: elements in the array
+ * @size: size of individual elements
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmeb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmeb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memtest(ptr, 0, nmeb * size);
+
+	return (ptr);
+}
