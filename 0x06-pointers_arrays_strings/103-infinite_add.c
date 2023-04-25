05cfc530c3fec62765e1da36bab612316e76db87
JosephJere93 <josephjere93@gmail.com>
2023-04-25 05:24:08 -0700

    Main files

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+        int i = 0;
+        int j = 0;
+        char temp;
+
+        while (*(n + i) != '\0')
+        {
+                i++;
+        }
+        i--;
+
+        for (j = 0; j < i; j++, i--)
+        {
+                temp = *(n + j);
+                *(n + j) = *(n + i);
+                *(n + i) = temp;
         }
 }
 
@@ -26,7 +51,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
         while (j >= 0 || i >= 0 || overflow == 1)
         {
                 if (i < 0)
-                        val1 = 0;
+			 val1 = 0;
                 else
                         val1 = *(n1 + i) - '0';
                 if (j < 0)
---
+++ README.md
@@ -0,0 +1,12 @@
+0x0A. C - argc, argv
+C
+ By: Julien Barbier
+ Weight: 1
+ Project over - took place from Apr 6, 2023 5:00 AM to Apr 11, 2023 5:00 AM
+ An auto review will be launched at the deadline
+In a nutshell…
+Auto QA review: 0.0/37 mandatory & 0.0/9 optional
+Altogether:  0.0%
+Mandatory: 0.0%
+Optional: 0.0%
+Calculation:  0.0% + (0.0% * 0.0%)  == 0.0%
