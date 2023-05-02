8258ae36eef2477cf7498b5b961dbc5e0e614d66
JosephJere93 <josephjere93@gmail.com>
2023-05-02 04:06:16 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,39 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- :wq
- *
+ * *_memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: character to copy
+ * @n: number of times to copy
+ * Return: pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
